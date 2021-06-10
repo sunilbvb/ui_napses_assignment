efa603afb449feebf4fb99523dff7fcfe0c1b264
Sunil Bakale <sunil@theblok.com>
2021-06-10 22:07:37 +0530

    implemented drawer icon

--- lib/constants/globals.dart
+++ lib/constants/globals.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 const tripSheetsAppBarTitle = 'Tripsheets';
+const Color kDarkBlueColor = Color(0xff0000FF);
+const Color kBlueColor = Colors.blue;
 const Color kBlackColor = Colors.black;
 const Color kWhiteColor = Colors.white;
+const Color kGreyColor = Colors.grey;
 const Color kNavBarSelectedColor = Color(0xff040307);
 const Color kNavBarStrokeColor = Color(0x30040307);
 const Color kNavBarUnSelectedColor = Color(0xffacacac);
-const Color kBlueColor = Color(0xff0000FF);
 
 const List<String> bottomNavBarText = [
   'Home',
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Napses UI assignment',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: kBlueColor,
       ),
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
@@ -35,6 +35,18 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       bottomNavigationBar: buildLightDesign(),
       appBar: AppBar(
+        toolbarHeight: 80,
+        leading: Container(
+          margin: const EdgeInsets.only(left: 10),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: kGreyColor, width: 1),
+          ),
+          child: IconButton(
+            icon: Icon(Icons.menu).iconColor(kBlackColor),
+            onPressed: () => null,
+          ),
+        ),
         elevation: 0,
         foregroundColor: kBlackColor,
         backgroundColor: kWhiteColor,
@@ -53,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
       height: 100,
       child: CustomNavigationBar(
         iconSize: 30.0,
-        selectedColor: kBlueColor,
+        selectedColor: kDarkBlueColor,
         strokeColor: kNavBarStrokeColor,
         unSelectedColor: kNavBarUnSelectedColor,
         backgroundColor: kWhiteColor,
@@ -61,40 +73,45 @@ class _MyHomePageState extends State<MyHomePage> {
           CustomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
             title: Text(bottomNavBarText[0])
-                .textColor(
-                    _currentIndex == 0 ? kBlueColor : kNavBarUnSelectedColor)
+                .textColor(_currentIndex == 0
+                    ? kDarkBlueColor
+                    : kNavBarUnSelectedColor)
                 .fontSize(12)
                 .paddingDirectional(vertical: 10),
           ),
           CustomNavigationBarItem(
             icon: Icon(Icons.note_outlined),
             title: Text(bottomNavBarText[1])
-                .textColor(
-                    _currentIndex == 1 ? kBlueColor : kNavBarUnSelectedColor)
+                .textColor(_currentIndex == 1
+                    ? kDarkBlueColor
+                    : kNavBarUnSelectedColor)
                 .fontSize(12)
                 .paddingDirectional(vertical: 10),
           ),
           CustomNavigationBarItem(
             icon: Icon(Icons.check_box_outlined),
             title: Text(bottomNavBarText[2])
-                .textColor(
-                    _currentIndex == 2 ? kBlueColor : kNavBarUnSelectedColor)
+                .textColor(_currentIndex == 2
+                    ? kDarkBlueColor
+                    : kNavBarUnSelectedColor)
                 .fontSize(12)
                 .paddingDirectional(vertical: 10),
           ),
           CustomNavigationBarItem(
             icon: Icon(Icons.account_balance_wallet_outlined),
             title: Text(bottomNavBarText[3])
-                .textColor(
-                    _currentIndex == 3 ? kBlueColor : kNavBarUnSelectedColor)
+                .textColor(_currentIndex == 3
+                    ? kDarkBlueColor
+                    : kNavBarUnSelectedColor)
                 .fontSize(12)
                 .paddingDirectional(vertical: 10),
           ),
           CustomNavigationBarItem(
             icon: Icon(Icons.people_outline),
             title: Text(bottomNavBarText[4])
-                .textColor(
-                    _currentIndex == 4 ? kBlueColor : kNavBarUnSelectedColor)
+                .textColor(_currentIndex == 4
+                    ? kDarkBlueColor
+                    : kNavBarUnSelectedColor)
                 .fontSize(12)
                 .paddingDirectional(vertical: 10),
           ),
