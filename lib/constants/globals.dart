8a8d6156d26a728bd337adb6abea9f363e6a1fdf
Sunil Bakale <sunil@theblok.com>
2021-06-10 21:34:36 +0530

    implemented bottom navigation bar

---
+++ lib/constants/globals.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const tripSheetsAppBarTitle = 'Tripsheets';
+const Color kBlackColor = Colors.black;
+const Color kWhiteColor = Colors.white;
+const Color kNavBarSelectedColor = Color(0xff040307);
+const Color kNavBarStrokeColor = Color(0x30040307);
+const Color kNavBarUnSelectedColor = Color(0xffacacac);
+const Color kBlueColor = Color(0xff0000FF);
+
+const List<String> bottomNavBarText = [
+  'Home',
+  'Orders',
+  'Tripsheets',
+  'Payments',
+  'Customers'
+];
