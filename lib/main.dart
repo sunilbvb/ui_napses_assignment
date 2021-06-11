3cefcfd4ff2cbf2f1ffc4a8eab88d0d012f3ed67
Sunil Bakale <sunil@theblok.com>
2021-06-11 11:21:05 +0530

    added list of listTiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:custom_navigation_bar/custom_navigation_bar.dart';
+import 'package:dropdownfield/dropdownfield.dart';
 import 'package:flutter/material.dart';
 import 'package:styled_widget/styled_widget.dart';
 
@@ -30,8 +31,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
   String _dropDownValue = 'All';
-  List<String> _dropDownValues = ['All']; // Option 2
-  // String _selectedLocation; // Option 2
+  List<String> _dropDownValues = ['All', 'All Agents']; // Option 2
+  String _dropDownValue2 = 'All Agents';
 
   @override
   Widget build(BuildContext context) {
@@ -63,77 +64,149 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(tripSheetsAppBarTitle).fontSize(25).textColor(kBlackColor),
       ),
       backgroundColor: kWhiteColor,
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            height: 90,
-            width: 150,
-            decoration: BoxDecoration(
-              color: kLightGrey,
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Delivery Date'),
-                Text(
-                  '27/05/2021',
-                ).fontSize(20).fontWeight(FontWeight.bold),
-              ],
-            ),
-          ),
-          Container(
-            height: 90,
-            width: 150,
-            decoration: BoxDecoration(
-              color: kLightGrey,
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child:
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                const Text('Tripsheet Status'),
-/*                Text('All')
-                    .fontSize(20)
-                    .fontWeight(FontWeight.bold)
-                    .alignment(AlignmentDirectional.centerStart)
-                    .padding(left: 25),*/
-                Container(
-                  width: 100,
-                  padding: EdgeInsets.only(bottom: 10,),
-                  child: DropdownButton(
-                    isExpanded: true,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: kBlackColor,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 90,
+                      decoration: BoxDecoration(
+                        color: kLightGrey,
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Delivery Date').padding(left: 25),
+                          Text(
+                            '27/05/2021',
+                          )
+                              .fontSize(20)
+                              .fontWeight(FontWeight.bold)
+                              .padding(left: 25),
+                        ],
+                      ),
                     ),
-                    iconEnabledColor: Colors.redAccent,
-                    underline: SizedBox(),
-                    icon: Icon(Icons.arrow_drop_down),
-                    value: _dropDownValue ?? Text('All'),
-                    onChanged: (newValue) {
-                      setState(
-                        () {
-                          _dropDownValue = newValue;
-                        },
-                      );
-                    },
-                    items: _dropDownValues.map(
-                      (value) {
-                        return DropdownMenuItem(
-                          child: Text(value),
-                          value: value,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 90,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: kLightGrey,
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Tripsheet Status').padding(left: 25),
+                          Container(
+                            width: double.infinity,
+                            padding: EdgeInsets.only(
+                              bottom: 5,
+                            ),
+                            child: DropdownButton(
+                              isExpanded: true,
+                              iconSize: 30,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: kBlackColor,
+                              ),
+                              iconEnabledColor: kDarkBlueColor,
+                              underline: SizedBox(),
+                              icon: Icon(Icons.keyboard_arrow_down_outlined),
+                              value: _dropDownValue ?? Text('All'),
+                              onChanged: (newValue) {
+                                setState(
+                                  () {
+                                    _dropDownValue = newValue;
+                                  },
+                                );
+                              },
+                              items: _dropDownValues.map(
+                                (value) {
+                                  return DropdownMenuItem(
+                                    child: Text(value),
+                                    value: value,
+                                  );
+                                },
+                              ).toList(),
+                            ).padding(horizontal: 26),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: kLightGrey,
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                padding: EdgeInsets.all(15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Select Agent').padding(left: 10),
+                    DropdownButton(
+                      iconSize: 30,
+                      isExpanded: true,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: kBlackColor,
+                      ),
+                      iconEnabledColor: kDarkBlueColor,
+                      underline: SizedBox(),
+                      icon: Icon(Icons.keyboard_arrow_down_outlined),
+                      value: _dropDownValue2,
+                      onChanged: (newValue) {
+                        setState(
+                          () {
+                            _dropDownValue2 = newValue;
+                          },
                         );
                       },
-                    ).toList(),
-                  ),
+                      items: _dropDownValues.map(
+                        (value) {
+                          return DropdownMenuItem(
+                            child: Text(value),
+                            value: value,
+                          );
+                        },
+                      ).toList(),
+                    ).padding(horizontal: 12),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Column(
+                children: listOfListTiles(),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
@@ -204,5 +277,235 @@ class _MyHomePageState extends State<MyHomePage> {
         },
       ),
     );
+  }
+
+  List<Widget> listOfListTiles() {
+    return [
+      Container(
+        decoration: BoxDecoration(
+          color: kLightOrangeColor,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+        child: ListTile(
+          leading: Container(
+            margin: const EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: Border.all(color: kGreyColor),
+            ),
+            child: Container(
+              color: kWhiteColor,
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '27 ',
+                    ).fontWeight(FontWeight.bold).fontSize(12),
+                    TextSpan(text: '\nMAY ').fontSize(10),
+                  ],
+                ),
+              ).padding(all: 5),
+            ),
+          ),
+          subtitle: Text('ORDERS: 20 . AGENT: DINESH KUMAR').fontSize(10),
+          title: Text(
+            'Karolibaugh 1',
+            textScaleFactor: 1.0,
+          ).fontWeight(FontWeight.bold),
+          trailing: Icon(Icons.done),
+        ),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: kLightOrangeColor,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+        child: ListTile(
+          leading: Container(
+            margin: const EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: Border.all(color: kGreyColor),
+            ),
+            child: Container(
+              color: kWhiteColor,
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '27 ',
+                    ).fontWeight(FontWeight.bold).fontSize(12),
+                    TextSpan(text: '\nMAY ').fontSize(10),
+                  ],
+                ),
+              ).padding(all: 5),
+            ),
+          ),
+          subtitle: Text('ORDERS: 20 . AGENT: DINESH KUMAR').fontSize(10),
+          title: Text(
+            'Karolibaugh 1',
+            textScaleFactor: 1.0,
+          ).fontWeight(FontWeight.bold),
+          trailing: Icon(Icons.done),
+        ),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: kLightOrangeColor,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+        child: ListTile(
+          leading: Container(
+            margin: const EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: Border.all(color: kGreyColor),
+            ),
+            child: Container(
+              color: kWhiteColor,
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '27 ',
+                    ).fontWeight(FontWeight.bold).fontSize(12),
+                    TextSpan(text: '\nMAY ').fontSize(10),
+                  ],
+                ),
+              ).padding(all: 5),
+            ),
+          ),
+          subtitle: Text('ORDERS: 20 . AGENT: DINESH KUMAR').fontSize(10),
+          title: Text(
+            'Karolibaugh 1',
+            textScaleFactor: 1.0,
+          ).fontWeight(FontWeight.bold),
+          trailing: Icon(Icons.done),
+        ),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: kLightOrangeColor,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+        child: ListTile(
+          leading: Container(
+            margin: const EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: Border.all(color: kGreyColor),
+            ),
+            child: Container(
+              color: kWhiteColor,
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '27 ',
+                    ).fontWeight(FontWeight.bold).fontSize(12),
+                    TextSpan(text: '\nMAY ').fontSize(10),
+                  ],
+                ),
+              ).padding(all: 5),
+            ),
+          ),
+          subtitle: Text('ORDERS: 20 . AGENT: DINESH KUMAR').fontSize(10),
+          title: Text(
+            'Karolibaugh 1',
+            textScaleFactor: 1.0,
+          ).fontWeight(FontWeight.bold),
+          trailing: Icon(Icons.done),
+        ),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: kLightOrangeColor,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+        child: ListTile(
+          leading: Container(
+            margin: const EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: Border.all(color: kGreyColor),
+            ),
+            child: Container(
+              color: kWhiteColor,
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '27 ',
+                    ).fontWeight(FontWeight.bold).fontSize(12),
+                    TextSpan(text: '\nMAY ').fontSize(10),
+                  ],
+                ),
+              ).padding(all: 5),
+            ),
+          ),
+          subtitle: Text('ORDERS: 20 . AGENT: DINESH KUMAR').fontSize(10),
+          title: Text(
+            'Karolibaugh 1',
+            textScaleFactor: 1.0,
+          ).fontWeight(FontWeight.bold),
+          trailing: Icon(Icons.done),
+        ),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: kLightOrangeColor,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+        child: ListTile(
+          leading: Container(
+            margin: const EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: Border.all(color: kGreyColor),
+            ),
+            child: Container(
+              color: kWhiteColor,
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '27 ',
+                    ).fontWeight(FontWeight.bold).fontSize(12),
+                    TextSpan(text: '\nMAY ').fontSize(10),
+                  ],
+                ),
+              ).padding(all: 5),
+            ),
+          ),
+          subtitle: Text('ORDERS: 20 . AGENT: DINESH KUMAR').fontSize(10),
+          title: Text(
+            'Karolibaugh 1',
+            textScaleFactor: 1.0,
+          ).fontWeight(FontWeight.bold),
+          trailing: Icon(Icons.done),
+        ),
+      ),
+    ];
   }
 }
